b3b51ac82553e8e0e86d14ecb119664fd0fee1b2
hacker777-cris <crispusgikonyo@gmail.com>
2025-04-13 20:50:14 +0300

    update

--- rental/settings.py
+++ rental/settings.py
@@ -122,6 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
+# Use relative path or BASE_DIR
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Instead of '/app/static'
+]
+
 STATIC_URL = "static/"
 
 # Default primary key field type

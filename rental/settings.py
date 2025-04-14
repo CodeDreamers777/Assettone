d36f9ff0e7d986ba505acbadaf45c9e6258ad5a1
hacker777-cris <crispusgikonyo@gmail.com>
2025-04-14 06:38:33 +0300

    update database to prod db

--- rental/settings.py
+++ rental/settings.py
@@ -13,10 +13,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Use environment variable for database URL with a fallback for development
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3")
 
 
 # Quick-start development settings - unsuitable for production
@@ -79,13 +86,18 @@ WSGI_APPLICATION = "rental.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+"""
+# postgreSQL
+
+
+DATABASES = {"default": dj_database_url.parse(DATABASE_URL)}
 
 
 # Password validation

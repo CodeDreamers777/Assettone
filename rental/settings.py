8a2cf8e043a7c4eb757d8416698b9e68da140bc9
hacker777-cris <crispusgikonyo@gmail.com>
2025-04-14 07:21:07 +0300

    update database to prod db

--- rental/settings.py
+++ rental/settings.py
@@ -97,7 +97,9 @@ DATABASES = {
 # postgreSQL
 
 
-DATABASES = {"default": dj_database_url.parse(DATABASE_URL)}
+DATABASES = {
+    "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+}
 
 
 # Password validation

66e47165111068073c193e6896f8bce79cda4e54
hacker777-cris <crispusgikonyo@gmail.com>
2025-04-14 08:08:21 +0300

    update database to prod db

--- rental/settings.py
+++ rental/settings.py
@@ -86,20 +86,19 @@ WSGI_APPLICATION = "rental.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-"""
 # postgreSQL
 
-
+"""
 DATABASES = {
     "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
 }
+    """
 
 
 # Password validation
